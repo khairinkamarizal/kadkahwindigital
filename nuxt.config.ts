53c405054f5adc82231839676b2ab6e0f2ab98a4
Khairin Kamarizal <khairinkamarizal@gmail.com>
2026-04-05 03:06:59 +0800

    lol

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,24 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      title: "Walimatul Urus Mimi & Hafiz",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" },
+        {
+          name: "description",
+          content: "Jemputan ke Majlis Perkahwinan Puteri Nurul Syamimi & Muhamad Hafiz pada 30 Mei 2026.",
+        },
+        { property: "og:title", content: "Walimatul Urus Mimi & Hafiz" },
+        {
+          property: "og:description",
+          content: "Jemputan ke Majlis Perkahwinan Puteri Nurul Syamimi & Muhamad Hafiz pada 30 Mei 2026.",
+        },
+        { property: "og:type", content: "website" },
+        { name: "theme-color", content: "#660B05" }
+      ],
       link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
         {
           rel: "preconnect",
